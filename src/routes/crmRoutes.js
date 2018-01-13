84fa59bdb3777107f21538d1aa61cd2b240e8fdb
PedroSouzaSilva <pedross.tool@gmail.com>
2018-01-13 10:23:07 -0200

    adicionando insert em banco

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,10 +1,18 @@
+import { addNewContact } from "../controllers/crmControllers";
+
 const routes = (app) => {
     app.route('/contact')
-    .get((req, res) => 
-    res.send('GET resquest successful!!!'))
+    .get((req, res, next) => {
+        //middleware
+        console.log(`Request from: ${req.originalUrl}`)
+        console.log(`Request from: ${req.method}`)
+        next();
+    }, (req, res, next) => {
+        res.send('GET resquest successful!!!');
+    })
     
-    .post((req, res) => 
-    res.send('POST resquest successful!!!'));
+    //POST endpoint
+    .post(addNewContact);
 
     app.route('/contact/:contactId')
     .put((req, res) => 
